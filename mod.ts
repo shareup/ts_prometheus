57ba35cf05c577abe96f2bb367a903ee4cc492d3
Nathan Herald <me@nathanherald.com>
2023-02-17 12:04:07 +0100

    Actually expose the exposition formatters

--- mod.ts
+++ mod.ts
@@ -4,4 +4,5 @@ export { Histogram } from "./histogram.ts";
 export { Summary } from "./summary.ts";
 export { DelayedAggregation } from "./delayed_aggregation.ts";
 export type { Dec, Inc, Metric, Observe, Set } from "./metric.ts";
+export { defaultExposition, rawOutputsExposition } from "./exposition.ts";
 export { Registry } from "./registry.ts";
