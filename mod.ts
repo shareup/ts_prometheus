44b6cddc2866ba8927463c0a88bc18f51cd32bf8
Nathan Herald <me@nathanherald.com>
2023-02-17 12:01:14 +0100

    Actually expose the new DelayedAggregation from mod.ts

--- mod.ts
+++ mod.ts
@@ -2,5 +2,6 @@ export { Counter } from "./counter.ts";
 export { Gauge } from "./gauge.ts";
 export { Histogram } from "./histogram.ts";
 export { Summary } from "./summary.ts";
+export { DelayedAggregation } from "./delayed_aggregation.ts";
 export type { Dec, Inc, Metric, Observe, Set } from "./metric.ts";
 export { Registry } from "./registry.ts";
